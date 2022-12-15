e759038c842588d51ffd17f3c6412347d6fb010f
souad988 <59707859+souad988@users.noreply.github.com>
2022-12-15 16:35:46 +0100

    Add signUp screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import LogIn from './LogIn';
-
+import SignUp from './SignUp';
 function Home() {
   return (
     <View>
       <Text>Home</Text>
-      <LogIn />
+      <SignUp />
     </View>
   );
 }
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,28 +5,44 @@ import CustomButton from '../components/CustomButton';
 
 function LogIn() {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const login = () => {};
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const signUp = () => {};
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
       <CustomInput
         placeholder="username"
         value={username}
         setValue={setUsername}
       />
+      <CustomInput placeholder="email" value={email} setValue={setEmail} />
       <CustomInput
         placeholder="password"
         value={password}
         setValue={setPassword}
       />
-      <CustomButton title="Login" onPress={login} color="#0093AB" />
-      <Text style={styles.link}>Forgot password -> </Text>
+      <CustomInput
+        placeholder="Confirm password"
+        value={confirmPassword}
+        setValue={setConfirmPassword}
+      />
+      <CustomButton title="SignUp" onPress={signUp} color="#FFD124" />
     </View>
   );
 }
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 50,
+    color: '#0093AB',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
   container: {
     marginHorizontal: 40,
+    marginVertical: 100,
   },
   link: {
     color: '#006778',
